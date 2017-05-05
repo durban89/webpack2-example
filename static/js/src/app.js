a32d44466a81ec4aa9633da6cb4f0902fc44ad3e
durban.zhang <durban.zhang@gmail.com>
2017-05-05 22:12:19 +0800

    add react-async-component to code split

--- static/js/src/app.js
+++ static/js/src/app.js
@@ -10,21 +10,30 @@ import { Provider } from 'react-redux';
 import createHistory from 'history/createBrowserHistory'
 import { ConnectedRouter } from 'react-router-redux'
 import configureStore from './store';
+import { asyncComponent } from 'react-async-component';
 
 const history = createHistory()
 const store = configureStore(history);
-
-import AppComponent from './components/AppComponent';
-import home from './home/stores/home';
-import topics from './home/stores/topics';
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
-        <Route exact path="/" component={AppComponent}/>
-        <Route path="/home" component={home}/>
-        <Route path="/topics" component={topics}/>
+        <Route exact path="/" component={asyncComponent({
+          resolve: () => System.import('./components/AppComponent'),
+          LoadingComponent:() => <div>Loading</div>,
+          ErrorComponent:(error)=><div>{error.message}</div>
+        })}/>
+        <Route path="/home" component={asyncComponent({
+          resolve: () => System.import('./home/stores/home'),
+          LoadingComponent:() => <div>Loading</div>,
+          ErrorComponent:(error)=><div>{error.message}</div>
+        })}/>
+        <Route path="/topics" component={asyncComponent({
+          resolve: () => System.import('./home/stores/topics'),
+          LoadingComponent:() => <div>Loading</div>,
+          ErrorComponent:(error)=><div>{error.message}</div>
+        })}/>
       </div>
     </ConnectedRouter>
   </Provider>,
